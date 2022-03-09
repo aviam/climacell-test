52aa3c609ef9d5d721dbe8ef38b5c7b668f1b8fb
Avi Amsalem <avshalom.amsalem@gmail.com>
2022-03-09 13:09:51 +0200

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import * as restify from 'express-restify-mongoose';
+import { User } from '@codespread/database-repo';
+
+export const users = Router();
+
+restify.serve(users, User, { prefix: '', version: '' });
